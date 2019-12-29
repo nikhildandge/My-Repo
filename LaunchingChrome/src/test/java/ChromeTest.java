966d0a278a9f7247b7fa02efddd00bc409d9789a
Nikhil Dandge <Nikhil Dandge@LAPTOP-BKTNALAK>
2019-12-29 17:10:07 +0530

    commit 4

--- LaunchingChrome/src/test/java/ChromeTest.java
+++ LaunchingChrome/src/test/java/ChromeTest.java
@@ -1,14 +1,17 @@
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class ChromeTest {
 	
 	@Test
-	public void chromeLaunchTest()
+	public void chromeLaunchTest() throws InterruptedException
 	{
-		FirefoxDriver driver = new FirefoxDriver();
+		ChromeDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.facebook.com");
+		Thread.sleep(3000);
+		driver.quit();
 	}
 
 }
